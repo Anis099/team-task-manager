7d6bf7adc60be1c8774fd7d9c16af7c07cc386be
Anis <shaikanis95@gmail.com>
2026-06-16 18:21:43 +0530

    config: update settings for production

--- core/settings.py
+++ core/settings.py
@@ -22,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-anyrandomstring123')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+DEBUG = False
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
